6b217bae8849f5d3bab77cf2db96481ad101a2df
Jason Cheng <jasoncheng224@gmail.com>
2023-04-23 07:36:59 -0700

    signup

--- frontend/src/SignUp/index.js
+++ frontend/src/SignUp/index.js
@@ -2,15 +2,18 @@ import { endpoint } from '../util';
 import './index.scss'
 import { useEffect, useState } from 'react';
 
+import CSRFToken from '../csrftoken';
+
 function SignUp() {
     //login for users
-    
+
 
     return (
         <form method="POST" action={endpoint("add-user")}>
-            <input name="username"></input>
-            <input type="password" name="password"></input>
+            <input name="username" />
+            <input type="password" name="password" />
             <button type="submit">Sign Up</button>
+            <CSRFToken />
         </form>
     );
 }
